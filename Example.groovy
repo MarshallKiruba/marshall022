066d4d259e7eab66d2ae11aae3ed9e4b28e4d59f
Muruganandam Nagalingam <muruganandam.nagalingam@tcs.com>
2017-12-27 15:40:48 +0530

    Changed the code.

--- Example.groovy
+++ Example.groovy
@@ -1,5 +1,8 @@
 class Example {
    static void main(String[] args) {
-      println('Hello World');
+      def lst = [13, 25, 100, 14]; 
+      def sortedlist = lst.sort(); 
+      def size = sortedlist.size();
+      System.out.println (sortedlist[size-1]);
    }
 }
